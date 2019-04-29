f208e2e0c0ce626fdb7302446e373cf882337c35
Dan Palmer <dan@thread.com>
2019-04-29 23:16:41 +0100

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/posts/images`
       }
     },
     "gatsby-transformer-sharp",
