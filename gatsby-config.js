971241976d0dd9f3c2ad6a6d8bc37d98d7747808
Dan Palmer <dan@danpalmer.me>
2019-07-02 22:10:27 +1000

    Add a bunch of nice Gatsby featurey things

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,90 +1,93 @@
 module.exports = {
   siteMetadata: {
-    title: "Dan Palmer",
-    siteUrl: "https://danpalmer.me/",
-    description: "Personal site and blog"
+    title: 'Dan Palmer',
+    siteUrl: 'https://danpalmer.me/',
+    description: 'Personal site and blog',
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "Personal site and blog of Dan Palmer",
-        short_name: "Dan Palmer",
-        start_url: "/",
-        background_color: "#c8e1ff",
-        theme_color: "#c8e1ff",
-        display: "minimal-ui"
-      }
+        name: 'Personal site and blog of Dan Palmer',
+        short_name: 'Dan Palmer',
+        start_url: '/',
+        background_color: '#c8e1ff',
+        theme_color: '#c8e1ff',
+        display: 'minimal-ui',
+      },
     },
-    "gatsby-plugin-offline",
+    'gatsby-plugin-offline',
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 600,
               quality: 80,
-              withWebp: { quality: 80 }
-            }
+              withWebp: { quality: 80 },
+            },
           },
           {
-            resolve: "gatsby-remark-embed-youtube",
+            resolve: 'gatsby-remark-embed-youtube',
             options: {
               width: 600,
-              height: 338 // 16:9
-            }
+              height: 338, // 16:9
+            },
           },
-          "gatsby-remark-responsive-iframe",
-          "gatsby-remark-embed-gist",
+          'gatsby-remark-responsive-iframe',
+          'gatsby-remark-embed-gist',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-widows',
           {
-            resolve: "gatsby-remark-highlights",
+            resolve: 'gatsby-remark-highlights',
             options: {
-              additionalLangs: ["language-graphql", "language-haskell"],
-              scopePrefix: "syntax--",
+              additionalLangs: ['language-graphql', 'language-haskell'],
+              scopePrefix: 'syntax--',
               codeWrap: {
-                className: "snippet"
-              }
-            }
-          }
-        ]
-      }
+                className: 'snippet',
+              },
+            },
+          },
+        ],
+      },
     },
-    "gatsby-plugin-netlify",
+    'gatsby-plugin-netlify',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "src",
-        path: `${__dirname}/posts`
-      }
+        name: 'src',
+        path: `${__dirname}/posts`,
+      },
     },
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: 'gatsby-plugin-sass',
       options: {
-        precision: 8
-      }
+        precision: 8,
+      },
     },
-    "gatsby-transformer-yaml",
+    'gatsby-transformer-yaml',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: "./data/"
-      }
+        path: './data/',
+      },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: `${__dirname}/posts/images`
-      }
+        name: 'images',
+        path: `${__dirname}/posts/images`,
+      },
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    "gatsby-redirect-from",
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-redirect-from',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-feed"
-    }
-  ]
+      resolve: 'gatsby-plugin-feed',
+    },
+  ],
 };
