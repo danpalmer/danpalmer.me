8ca2e5a958bb6016c17e4d95889251e496953394
Dan Palmer <dan@danpalmer.me>
2019-07-05 13:01:35 +1000

    Formatting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/layout';
+import Layout from "../components/layout";
 
 const PostItem = ({ post }) => {
   return (
