c2308ff0513fe5d26576ee5b0564aaf52b60d134
Dan Palmer <dan@danpalmer.me>
2022-09-10 11:36:30 +0000

    Upgrade all the things

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,6 +100,7 @@ module.exports = {
       },
     },
     "gatsby-transformer-sharp",
+    "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-redirect-from",
     "gatsby-plugin-sitemap",
